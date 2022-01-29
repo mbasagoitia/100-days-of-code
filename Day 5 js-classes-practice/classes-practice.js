7d7029abcd605fe8120ecd2959410345f6f1620f
basagoitiam <marika.basagoitia@gmail.com>
2022-01-29 15:52:31 -0500

    more classes practice

--- Day 5 js-classes-practice/classes-practice.js
+++ Day 5 js-classes-practice/classes-practice.js
@@ -35,3 +35,61 @@ class Citrus extends Fruit {
 
 let orange = new Citrus("orange", "sweet");
 console.log(orange.describeFlavor());
+
+class Instrument {
+    constructor(instrument, yearsPlayed) {
+        this.instrument = instrument;
+        this.yearsPlayed = yearsPlayed;
+    }
+    plays () {
+        return `I have played the ${this.instrument} for ${this.yearsPlayed} years.`
+    }
+}
+
+const drums = new Instrument("drums", 10);
+console.log(drums.plays());
+
+class Family extends Instrument {
+    constructor(instrument, yearsPlayed, type) {
+        super(instrument, yearsPlayed);
+        this.type = type;
+    }
+    familyPlays () {
+        return `${this.plays()} It is a ${this.type} instrument.`;
+    }
+}
+
+const violin = new Family("violin", 17, "stringed");
+console.log(violin.familyPlays());
+console.log(violin);
+
+const piano = new Family("piano", 5, "percussion");
+console.log(piano.familyPlays());
+
+class User {
+    constructor(name, age, email) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+    welcomeMessage () {
+        return `Welcome to the site, ${this.name}!`;
+    }
+}
+
+class loggedInUser extends User {
+    constructor(name, age, email) {
+        super(name, age, email);
+    }
+    loginMessage () {
+        return `${super.welcomeMessage()} You are currently logged in.`
+    }
+}
+
+const john = new User("John", 25, "john@john.com");
+const mary = new User("Mary", 17, "mary@mary.com");
+const james = new loggedInUser("James", 28, "james@james.com");
+
+console.log(mary.welcomeMessage());
+console.log(john.welcomeMessage());
+console.log(james.loginMessage());
