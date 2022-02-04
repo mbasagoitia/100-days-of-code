566c68f83d241a46ee9d0c5851238f39f2bf7574
basagoitiam <marika.basagoitia@gmail.com>
2022-02-04 17:01:08 -0500

    added functionality to generate random name button

--- Day 9 random-name-generator/script.js
+++ Day 9 random-name-generator/script.js
@@ -5,19 +5,39 @@ const nameList = document.querySelector("#name-list");
 const generateButton = document.querySelector("#generate-button");
 const nameOutput = document.querySelector("#output");
 
-//when user clicks button, add name to name list
-//addNameButton.addEventListener("click", addToList(nameInput.value)); why isn't this working???
-//max 27 names will fit on the list, how do I do this
 
+//addNameButton.addEventListener("click", addToList(nameInput.value)); why isn't this working???
+
+//array to hold the names that have been added to the list to access later
+const nameArray = [];
+
+//when user clicks button, add name to name list
 function addToList(name) {
+    if (nameArray.length > 24) {
+        alert("Maximum number of names reached");
+    } else {
+    nameArray.push(name);
     let newName = document.createElement("li");
     newName.classList.add("list-item");
     let text = document.createTextNode(name);
     newName.appendChild(text);
     nameList.appendChild(newName);
     clearInput(); 
+    //console.log(nameArray);
+    }
 }
 
 function clearInput() {
     nameInput.value="";
+}
+
+//function to choose a random name from nameArray and display it in nameOutput
+function displayRandomName() {
+    if (nameArray.length === 0) {
+        alert("You must add at least one name to the list");
+    } else {
+    let randomIndex = Math.floor(Math.random()*(nameArray.length));
+    //console.log(randomIndex);
+    nameOutput.textContent = nameArray[randomIndex];
+    }
 }
