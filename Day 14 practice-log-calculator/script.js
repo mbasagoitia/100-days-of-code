3d3c0c901cd15374ee747b5325904d447def8565
basagoitiam <marika.basagoitia@gmail.com>
2022-02-11 18:54:07 -0500

    finished basic markup and structure, added date

--- Day 14 practice-log-calculator/script.js
+++ Day 14 practice-log-calculator/script.js
@@ -1,3 +1,5 @@
+//hide form/show chart button
+
 const form = document.querySelector(".form-content-wrapper");
 const button = document.querySelector("#show-chart-button");
 const chart = document.querySelector(".chart-content-wrapper");
@@ -12,3 +14,13 @@ function showForm () {
     chart.classList.add("hidden");
 }
 
+//get current date and display it in the title
+let date = new Date();
+let day = date.getDate();
+let longMonth = date.toLocaleString('en-us', { month: 'long' });
+let year = date.getFullYear();
+
+let fullDate = `${longMonth} ${day}, ${year}`;
+
+const dateDisplay = document.querySelector(".date");
+dateDisplay.innerText = fullDate;   
