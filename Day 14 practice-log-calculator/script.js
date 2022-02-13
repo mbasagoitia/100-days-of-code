3f2f58e3e26acba4be6cb21c1c72d897caf75fba
basagoitiam <marika.basagoitia@gmail.com>
2022-02-12 21:08:41 -0500

    added functionality to some buttons

--- Day 14 practice-log-calculator/script.js
+++ Day 14 practice-log-calculator/script.js
@@ -1,3 +1,14 @@
+//get current date and display it in the title
+let date = new Date();
+let day = date.getDate();
+let longMonth = date.toLocaleString('en-us', { month: 'long' });
+let year = date.getFullYear();
+
+let fullDate = `${longMonth} ${day}, ${year}`;
+
+const dateDisplay = document.querySelector(".date");
+dateDisplay.innerText = fullDate;  
+
 //hide form/show chart button
 
 const form = document.querySelector(".form-content-wrapper");
@@ -14,13 +25,53 @@ function showForm () {
     chart.classList.add("hidden");
 }
 
-//get current date and display it in the title
-let date = new Date();
-let day = date.getDate();
-let longMonth = date.toLocaleString('en-us', { month: 'long' });
-let year = date.getFullYear();
+//set checkmark background on empty squares
+const emptyBox = document.querySelectorAll(".empty-space");
 
-let fullDate = `${longMonth} ${day}, ${year}`;
+emptyBox.addEventListener("click", function () {
+    emptyBox.forEach(element => element.addEventListener("click", function () {
+        element.classList.add("check-background");
+    }))
+})
 
-const dateDisplay = document.querySelector(".date");
-dateDisplay.innerText = fullDate;   
+//calculate total practice time
+const totalHours = document.querySelector("#practice-time-hours");
+const totalMins = document.querySelector("#practice-time-minutes");
+
+let totalPracticeTime;
+
+totalHours.addEventListener("input", function () {
+    if (!totalHours.value) {
+        totalPracticeTime = parseInt(totalMins.value);
+        //console.log(totalPracticeTime);
+    } else if (!totalMins.value) {
+        totalPracticeTime = parseInt(totalHours.value)*60;
+        //console.log(totalPracticeTime);
+    } else {
+    totalPracticeTime = parseInt(totalHours.value)*60 + parseInt(totalMins.value);
+    //console.log(totalPracticeTime);
+    }
+})
+
+totalMins.addEventListener("input", function () {
+    if (!totalHours.value) {
+        totalPracticeTime = parseInt(totalMins.value);
+        //console.log(totalPracticeTime);
+    } else if (!totalMins.value) {
+        totalPracticeTime = parseInt(totalHours.value)*60;
+        //console.log(totalPracticeTime);
+    } else {
+    totalPracticeTime = parseInt(totalHours.value)*60 + parseInt(totalMins.value);
+    //console.log(totalPracticeTime);
+    }
+})
+
+//get warmup time
+
+const warmup = document.querySelector("#warmup-input");
+let warmupTime;
+
+warmup.addEventListener("input", function () {
+    warmupTime = parseInt(warmup.value);
+    //console.log(warmupTime);
+})
