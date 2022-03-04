997559ba7bf799fc91cebebad3974a4efed629a4
basagoitiam <marika.basagoitia@gmail.com>
2022-03-03 21:17:36 -0500

    figured out image upload process

--- Day 36 photo-board-app/script.js
+++ Day 36 photo-board-app/script.js
@@ -9,21 +9,42 @@ $(document).ready(function () {
         $(".large-image").attr("src", "#");
     })
     //create new blank small image
-    function createSmallImage () {
+    const fileInput = document.querySelector("#file");
 
-        let newImage = $(document.createElement("img"));
-        newImage.attr("src", "#");
-        newImage.attr("class", "small-image");
-        let newWrapper = $(document.createElement("div"));
-        newWrapper.attr("class", "small-image-wrapper");
-        let newBorder = $(document.createElement("div"));
-        newBorder.attr("class", "small-image-border");
+    fileInput.addEventListener("change", function(e) {
+
+    let newImage = new Image(100, 100);
+  
+    newImage.src = e.target.files[0];
+    newImage.src = URL.createObjectURL(e.target.files[0]);
+    console.log(newImage.src);
         
-        newBorder.appendTo(".image-slider");
-        newWrapper.appendTo(newBorder);
-        newImage.appendTo(newWrapper);
-    }
-    createSmallImage();
-    createSmallImage();
+    $(newImage).attr("class", "small-image");
+    let newWrapper = $(document.createElement("div"));
+    $(newWrapper).attr("class", "small-image-wrapper");
+    let newBorder = $(document.createElement("div"));
+    $(newBorder).attr("class", "small-image-border");
+    URL.revokeObjectURL(newImage.src);
+    $(newBorder).appendTo(".image-slider");
+    $(newWrapper).appendTo(newBorder);
+    $(newImage).appendTo(newWrapper);
+  });
+  
 })
+
+/* function uploadImage() {
+    let fileInput = document.querySelector("#file");
+    let selectedFile = fileInput.files[0];
+    let newImage = $(document.createElement("img"));
+    newImage.attr("src", URL.createObjectURL(selectedFile));
+    console.log(selectedFile);
+    newImage.attr("class", "small-image");
+    let newWrapper = $(document.createElement("div"));
+    newWrapper.attr("class", "small-image-wrapper");
+    let newBorder = $(document.createElement("div"));
+    newBorder.attr("class", "small-image-border");
+    newBorder.appendTo(".image-slider");
+    newWrapper.appendTo(newBorder);
+    newImage.appendTo(newWrapper);
+} */
 
