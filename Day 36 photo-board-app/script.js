aa5f956719dcfdc3ad10e2ad3d15ccd4790a4a71
basagoitiam <marika.basagoitia@gmail.com>
2022-03-06 20:21:10 -0500

    added background image and fixed image move button

--- Day 36 photo-board-app/script.js
+++ Day 36 photo-board-app/script.js
@@ -1,32 +1,32 @@
 $(document).ready(function () {
     //when a small image is clicked, fill large image with its photo
     $(".image-slider").on("click", ".small-image", function () {
-        //console.log("clicked");
+        $(".large-image[src='#']").first().removeAttr("style");
         let source = $(this).attr("src");
-        $(".large-image[src='#']").first().attr("src", source);
+        $(".large-image[src='#']").first().attr("src", source);       
     })
     //reset photos
     $("#reset-photos-button").click(function() {
         $(".large-image").attr("src", "#");
     })
-    //create new blank small image
 
 
 //get random image from unsplash
+
+$("#add-image-button").on("click", function (){
+    getNewPhoto();
+});
+
 const getNewPhoto = async function () {
     const res = await fetch(
-     "https://api.unsplash.com/search/photos?query=nature&client_id=ym4WDstZ94HOSUUObxZBmX5b6P2lAgs3ma34iqIWFAM"
+     `https://api.unsplash.com/search/photos?query=${$("#searchbar").val()}&client_id=ym4WDstZ94HOSUUObxZBmX5b6P2lAgs3ma34iqIWFAM`
     );
     const images = await res.json();
     console.log(images);
     generateRandomImage(images);
   };
-  getNewPhoto();
-  getNewPhoto();
-  getNewPhoto();
-  getNewPhoto();
-  getNewPhoto();
-  getNewPhoto();
+
+
 
 function generateRandomImage(images) {
     let randomIndex = Math.floor(Math.random()*images.results.length);
@@ -34,8 +34,6 @@ function generateRandomImage(images) {
     let imageAddress = randomImage.urls.full;   
     let newImage = $(document.createElement("img"));
     $(newImage).attr("src", imageAddress);
-    console.log(imageAddress);
-    //i think the problem is here
     $(newImage).attr("class", "small-image");
     let newWrapper = $(document.createElement("div"));
     $(newWrapper).attr("class", "small-image-wrapper");
@@ -45,6 +43,14 @@ function generateRandomImage(images) {
     $(newWrapper).appendTo(newBorder);
     $(newImage).appendTo(newWrapper);
 
-}
+    }
+
+    //create photo display
+    $("#create-display-button").on("click", function(){
+        //hide main area eventually
+        console.log("clicked");
+        $(".small-image-border").appendTo($("bg-img-wrapper"));
+    })
+
 
 })
