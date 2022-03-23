51567d3d5842e19ccda4e6640d65bd469614dcaf
basagoitiam <marika.basagoitia@gmail.com>
2022-03-23 15:53:07 -0400

    fixed image captions, made skeleton for drag and drop game

--- Day 56 drag-and-drop-game/script.js
+++ Day 56 drag-and-drop-game/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
     $("img,.img-caption").hover(function() {
         $(this).parent().find(".img-caption").toggleClass("hidden");
+        $(this).parent().find(".img-caption").toggleClass("inline");
     })
     $(".img-caption").hover(function () {
         $(this).parent().find("img").toggleClass("transparent");
     })
-    
+    $(".img-wrapper").click(function(){
+        $("#choose-game-page").hide();
+    })
+    $(".challenge-card").draggable();
+
+    $(".target-card").droppable();
 })
