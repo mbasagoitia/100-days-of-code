7f95c78b423bfad4c7b5346abe2b362df0d95dcf
basagoitiam <marika.basagoitia@gmail.com>
2022-03-26 10:20:55 -0400

    made cards draggable and continued styling

--- Day 56 drag-and-drop-game/script.js
+++ Day 56 drag-and-drop-game/script.js
@@ -1,3 +1,27 @@
+const instruments = [
+   {item: "violin", answer: "string", src: "#"},
+   {item: "viola", answer: "string"},
+   {item: "cello", answer: "string"},
+   {item: "double bass", answer: "string"},
+   {item: "trumpet", answer: "brass"},
+   {item: "trombone", answer: "brass"},
+   {item: "tuba", answer: "brass"},
+   {item: "euphonium", answer: "brass"},
+   {item: "flute", answer: "woodwind"},
+   {item: "oboe", answer: "woodwind"},
+   {item: "clarinet", answer: "woodwind"},
+   {item: "bassoon", answer: "woodwind"},
+   {item: "saxophone", answer: "woodwind"},
+   {item: "piano", answer: "keyboard"},
+   {item: "harpsichord", answer: "keyboard"},
+   {item: "celesta", answer: "keyboard"},
+   {item: "clavichord", answer: "keyboard"},
+   {item: "snare drum", answer: "percussion"},
+   {item: "timpani", answer: "percussion"},
+   {item: "bongos", answer: "percussion"},
+   {item: "triangle", answer: "percussion"},
+]
+
 $(document).ready(function(){
     $("img,.img-caption").hover(function() {
         $(this).parent().find(".img-caption").toggleClass("hidden");
@@ -8,8 +32,12 @@ $(document).ready(function(){
     })
     $(".img-wrapper").click(function(){
         $("#choose-game-page").hide();
+        //create cards with a class based upon their answer for the droppable to accept
     })
+
     $(".challenge-card").draggable();
 
-    $(".target-card").droppable();
+    /* $(".target-card(specify which one)").droppable({
+        accept:
+    }); */
 })
